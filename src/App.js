8cebba0dd7dd142fbbfdebe1e6942fbcdae2a39d
Kumari-Smitha <kumarismitha.dev@gmail.com>
2022-09-10 19:25:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  const emojipedia = {
+    "🤪": "Zany Face",
+    "🤑": "Money-Mouth Face",
+    "🤫": "Shushing Face",
+    "🤔": "Thinking Face",
+    "🤐": "Zipper-Mouth Face",
+    "😐": "Neutral Face",
+    "😑": "Expressionless Face",
+    "🤥": "Lying Face",
+    "😪": "Sleepy Face",
+    "🤤": "Drooling Face"
+  };
+
+  const emojiArray = Object.keys(emojipedia);
+
+  const showMeaning = (emoji) => {
+    var emojiMeaning = emojipedia[emoji];
+    setMeaning(emojiMeaning);
+  };
+
+  const handleChange = (e) => {
+    var inputEmoji = e.target.value;
+    var emojiMeaning = emojipedia[inputEmoji];
+    if (emojiMeaning === undefined) {
+      emojiMeaning = "Sorry! could not find it in database.";
+    }
+    setMeaning(emojiMeaning);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="center">
+      <div className="emojipedia">
+        <h1>Emojipedia</h1>
+        <img
+          style={{height:"3rem",marginTop:"1rem"}}
+          src="https://emojipedia.org/static/0c93ef5f62ceb11bf902.png"
+          alt="emojipedia"
+        />
+      </div>
+      <br></br>
+      <input onChange={handleChange} placeholder="Search Emojipedia"/>
+      <p style={{fontSize:"1.5rem"}}><strong>{meaning}</strong></p>
+      <h4>Emojis already you know</h4>
+      {emojiArray.map((emoji) => {
+        return (
+          <span
+          
+            key={emoji}
+            style={{ cursor: "pointer", fontSize: "2.5rem" }}
+            onClick={() => showMeaning(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+      </div>
     </div>
   );
 }
-
-export default App;
